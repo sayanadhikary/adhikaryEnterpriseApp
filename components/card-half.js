5b6a1c604bee70400e25658e0d74000a3cbd3340
sayan <sayan711226@gmail.com>
2024-08-14 23:05:35 +0530

    add rupee symbol

--- components/card-half.js
+++ components/card-half.js
@@ -5,7 +5,7 @@ return (
     <div className="my-3 mx-2 w-24 h-40 shadow-md shadow-gray-700 text-center">
         <Image className="mx-auto" src={image} alt="Product_Image" width={300} height={300} quality={100} />
         <p className="p-1 text-xs text-nowrap font-semibold text-ellipsis overflow-hidden">{name}</p>
-        <p className="p-1 font-bold">{price}</p>
+        <p className="p-1 font-bold">₹{price}</p>
       </div>
 )
 }

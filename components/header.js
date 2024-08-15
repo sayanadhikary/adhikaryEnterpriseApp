6b6cb8cced5ed9fe8f24e789002a5f74bf91a117
sayan <sayan711226@gmail.com>
2024-08-15 08:42:13 +0530

    add logo

--- components/header.js
+++ components/header.js
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Header(){
     return <>
     <div className="flex justify-between bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-t-xl fixed top-0 left-0 right-0">
-        <Link className="p-2 " href={"/"}>Adhikary Enterprise</Link>
+        <Link className="flex p-2" href={"/"}>
+        <Image className="mx-1" src={"/vercel.svg"} alt="Logo" width={22} height={20} />
+         Adhikary Enterprise</Link>
     </div>
     </>
 }

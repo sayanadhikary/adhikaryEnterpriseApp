17aa20c3a78c23db5e8a1b7e36f05192e4bc70c1
saynadhikary <sayan711226@gmail.com>
2024-08-24 17:36:44 +0530

    add loading and error page

---
+++ app/global-error.js
@@ -0,0 +1,14 @@
+'use client' // Error boundaries must be Client Components
+ 
+export default function GlobalError({ error, reset }) {
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body>
+        <h2>Something went wrong! Sorry from Adhikary Enterprise</h2>
+        <p>Message: {error}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
---
+++ app/loading.js
@@ -0,0 +1,4 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return <p className="text-center mt-36">Loading Adhikary Enterprise...</p>
+  }

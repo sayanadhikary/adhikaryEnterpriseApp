eee987d2123ecd035e577f8ff7b67fe315d66da1
saynadhikary <sayan711226@gmail.com>
2024-08-24 18:07:35 +0530

    modify error page

--- app/global-error.js
+++ app/global-error.js
@@ -4,10 +4,10 @@ export default function GlobalError({ error, reset }) {
   return (
     // global-error must include html and body tags
     <html>
-      <body>
-        <h2>Something went wrong! Sorry from Adhikary Enterprise</h2>
-        <p>Message: {error.message}</p>
-        <button onClick={() => reset()}>Try again</button>
+      <body className="text-center">
+        <h2 className="mt-36 p-6">Something went wrong! Sorry from Adhikary Enterprise</h2>
+        <p className="p-6">Message: {error.message}</p>
+        <button className="p-6 bg-green-700" onClick={() => reset()}>Try again</button>
       </body>
     </html>
   )

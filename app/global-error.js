ce6612760d6a553dc28c372bde0ecb1f6b03097d
saynadhikary <sayan711226@gmail.com>
2024-08-24 18:19:05 +0530

    modify error page

--- app/global-error.js
+++ app/global-error.js
@@ -1,4 +1,5 @@
 'use client' // Error boundaries must be Client Components
+import Link from "next/link"
  
 export default function GlobalError({ error, reset }) {
   return (
@@ -7,7 +8,8 @@ export default function GlobalError({ error, reset }) {
       <body className="text-center">
         <h2 className="mt-36 p-6">Something went wrong! Sorry from Adhikary Enterprise</h2>
         <p className="p-6">Message: {error.message}</p>
-        <button className="p-6 bg-green-700" onClick={() => reset()}>Try again</button>
+        <button className="p-4 bg-yellow-700 rounded-md" onClick={() => reset()}>Try again</button>
+        <Link className="p-4 bg-green-700 rounded-md" href={"/"}>Go Home</Link>
       </body>
     </html>
   )
